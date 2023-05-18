ecab90e9ea697ccd99bbb90463d8846e0e2f2216
ajoedv <>
2023-05-18 10:16:42 +0000

    Define the function to get the player's name and return it

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 import random
 import questions
+
+
+def get_player_name():
+        player_name = input("Enter your name: ")
+        print(player_name)
+        return get_player_name()
+get_player_name()
