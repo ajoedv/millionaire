8ac29f578ed69853875276eb5b6975d2212b1fc4
ajoedv <>
2023-05-18 11:19:13 +0000

    Define temporary questions and answers dictionary in question module

---
+++ questions.py
@@ -0,0 +1,13 @@
+# Define a list of questions with their corresponding answers
+questions = [
+    {
+        "question": "What is the capital city of Sweden?",
+        "options": ["a) London", "b) Stockholm", "c) Madrid", "d) Rome"],
+        "answer": "b"
+    },
+    {
+        "question": "What is the capital city of Spain?",
+        "options": ["a) London", "b) Stockholm", "c) Madrid", "d) Rome"],
+        "answer": "c"
+    },
+]
