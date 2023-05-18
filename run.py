ec8957c7d2b35788f01a2781c23c143decd55d6a
ajoedv <>
2023-05-18 10:35:48 +0000

    Validate player's name to be letters only

--- run.py
+++ run.py
@@ -7,7 +7,10 @@ def get_player_name():
         player_name = input("Enter your name: ")
         if len(player_name) == 0 or len(player_name) > 10:
             raise ValueError
-        return player_name.capitalize()
+        if not player_name.isalpha():
+            print("Invalid input! Please enter a valid name (letters only).")
+        else:
+            return player_name.capitalize()
     except ValueError:
         print("Invalid input! Please enter"
               "a valid name (up to 10 characters).")
