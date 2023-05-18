d187c446029e6639d819572c3a50e26be2c25af7
ajoedv <>
2023-05-18 10:29:58 +0000

    Validate player's name and handle invalid input

--- run.py
+++ run.py
@@ -3,7 +3,13 @@ import questions
 
 
 def get_player_name():
+    try:
         player_name = input("Enter your name: ")
-        print(player_name)
+        if len(player_name) == 0 or len(player_name) > 10:
+            raise ValueError
+        return player_name.capitalize()
+    except ValueError:
+        print("Invalid input! Please enter"
+              "a valid name (up to 10 characters).")
         return get_player_name()
 get_player_name()
