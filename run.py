3ff9e611a78666cda76c95e96061543ac285f3f0
ajoedv <>
2023-05-18 11:58:42 +0000

    add money_increments list to define money increment pattern

--- run.py
+++ run.py
@@ -49,3 +49,5 @@ def game_loop():
     # Shuffle the questions
     random.shuffle(questions.questions)
 
+    # money increment pattern
+    money_increments = [100, 200, 300, 500, 1000, 2000, 4000, 8000,16000, 32000, 64000, 125000, 250000, 500000, 1000000]
